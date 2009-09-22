38f40fda657562fc19e9fd17ef0457386e9e8925
Matthew S Burnside <msburns@moss-livestock.tycho.ncsc.mil>
2009-09-22 15:11:39 -0400

    Clean up whitespace

--- parse.h
+++ parse.h
@@ -4,30 +4,28 @@
 #define NSYMS 256
 
 struct symtab {
-     char *name;
+    char *name;
 };
 
 enum expr_type { VAR, OR_EXPR, AND_EXPR, NOT_EXPR, PAREN_EXPR };
 
 struct expr {
-	enum expr_type type;
-	union {
-		struct {
-			int sym;
-		} var;
-
-		struct {
-			struct expr *l;
-			struct expr *r;
-		} and, or;
-
-		struct {
-			struct expr *b;
-		} not, paren;
-	} u;
+    enum expr_type type;
+    union {
+        struct {
+            int sym;
+        } var;
+        struct {
+            struct expr *l;
+            struct expr *r;
+        } and, or;
+        struct {
+            struct expr *b;
+        } not, paren;
+    } u;
 };
 
-int             symbol(char *s);
+int				symbol(char *s);
 void            print_tree(struct expr *t);
 struct expr *   new_expr(enum expr_type type);
 
